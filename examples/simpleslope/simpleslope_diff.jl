bbd2dc9c6966996dffeb838b0649a5fb8cbe43cd
Alan Correa <correa@mbd.rwth-aachen.de>
2026-08-07 19:08:40 +0200

    update examples and solver printing

--- examples/simpleslope/simpleslope_diff.jl
+++ examples/simpleslope/simpleslope_diff.jl
@@ -1,12 +1,9 @@
-
-import FASolverAvalanche as FAS
+using GlissADe
 import ForwardDiff
 import LinearAlgebra
 
 ## HOW TO RUN THIS FILE? ##
-# Ensure that you're in the root folder "<something>/internship-tj
-# Run from terminal: "julia --project -t <num_threads> ./examples/simpleslope/simpleslope_diff.jl
-# Or Run from REPL as usual. If file not found error occurs, check if the file paths are correct.
+# From the project root: julia --project examples/simpleslope/simpleslope_diff.jl
 
 ## What does this file do? ##
 # This will showcase the library's forward mode automatic differentiation capabilities and
@@ -16,19 +13,18 @@ import LinearAlgebra
 ## Automatic Differentiation with Initial Conditions ##
 function testDifferentiabilityInitialConditions(x)
     rho = 1500.0
-    process = FAS.Process(threads = true, stats = true, plots = false, INT_TYPE = Int64)
-    FAS.init(process)
-    points, faces = FAS.parsemesh(
+    init(threads = true, stats = false, plots = false, int_type = Int64)
+    points, faces = parsemesh(
         "./examples/simpleslope/simpleslope/points",
         "./examples/simpleslope/simpleslope/faces",
         "./examples/simpleslope/simpleslope/faceLabels",
     )
-    Cells = FAS.preprocess(points, faces, eltype(x), comp_neighbours = false)
-    FAS.meshbounds(Cells)
-    polygon = FAS.findRegularPolygon([5.0, 10.0, -6.0, 6.0], npoints = 6)
-    cells_inside = FAS.cellsInsideBoundingPolygon(polygon, Cells)
-    FAS.initializeGeometry(cells_inside, Cells, rho, h0 = x[1], u0 = [0.0, 0.0, 0.0])
-    solution = FAS.Solution(
+    Cells = preprocess(points, faces, eltype(x), comp_neighbours = false)
+    meshbounds(Cells)
+    polygon = findRegularPolygon([5.0, 10.0, -6.0, 6.0], npoints = 6)
+    cells_inside = cellsInsideBoundingPolygon(polygon, Cells)
+    initializeGeometry(cells_inside, Cells, rho, h0 = x[1], u0 = [0.0, 0.0, 0.0])
+    solution = Solution(
         alpha = 0.5,
         zeta = 1.25,
         rho = 1500.0,
@@ -47,10 +43,10 @@ function testDifferentiabilityInitialConditions(x)
         points = points,
         faces = faces,
     )
-    solver = FAS.Solver(solution)
-    time_steps, sol = FAS.solve(solver, (0.0, 15.0), saveat = 0.2, Cₘ = 0.9)
-    FAS.writeToVTK(solution.location, sol, points, faces) # Save the solution
-    FAS.resetCells(Cells)
+    solver = Solver(solution)
+    time_steps, sol = solve(solver, (0.0, 15.0), saveat = 0.2, Cₘ = 0.9)
+    writeToVTK(solution.location, sol, points, faces) # Save the solution
+    resetCells(Cells)
     h = [sol[end][5*i-4] for i in eachindex(Cells)]
     return LinearAlgebra.norm2(h)/sqrt(length(h)) ## Average thickness at t = 15.0
 end
@@ -63,19 +59,18 @@ autodiff = ForwardDiff.gradient(testDifferentiabilityInitialConditions, [0.50])
 
 ## Automatic differentiation w.r.t model parameters ##
 function testDifferentiabilityInitialConditions(x)
-    process = FAS.Process(threads = true, stats = true, plots = false, INT_TYPE = Int64)
-    FAS.init(process)
-    points, faces = FAS.parsemesh(
+    init(threads = true, stats = false, plots = false, int_type = Int64)
+    points, faces = parsemesh(
         "./examples/simpleslope/simpleslope/points",
         "./examples/simpleslope/simpleslope/faces",
         "./examples/simpleslope/simpleslope/faceLabels",
     )
-    Cells = FAS.preprocess(points, faces, eltype(x), comp_neighbours = false)
-    FAS.meshbounds(Cells)
-    polygon = FAS.findRegularPolygon([5.0, 10.0, -6.0, 6.0], npoints = 6)
-    cells_inside = FAS.cellsInsideBoundingPolygon(polygon, Cells)
-    FAS.initializeGeometry(cells_inside, Cells, x[3], h0 = 0.2, u0 = [0.0, 0.0, 0.0])
-    solution = FAS.Solution(
+    Cells = preprocess(points, faces, eltype(x), comp_neighbours = false)
+    meshbounds(Cells)
+    polygon = findRegularPolygon([5.0, 10.0, -6.0, 6.0], npoints = 6)
+    cells_inside = cellsInsideBoundingPolygon(polygon, Cells)
+    initializeGeometry(cells_inside, Cells, x[3], h0 = 0.2, u0 = [0.0, 0.0, 0.0])
+    solution = Solution(
         alpha = x[1],
         zeta = x[2],
         rho = x[3],
@@ -94,10 +89,10 @@ function testDifferentiabilityInitialConditions(x)
         points = points,
         faces = faces,
     )
-    solver = FAS.Solver(solution)
-    time_steps, sol = FAS.solve(solver, (0.0, 15.0), saveat = 0.2, Cₘ = 0.9)
-    FAS.writeToVTK(solution.location, sol, points, faces) # Save the solution
-    FAS.resetCells(Cells)
+    solver = Solver(solution)
+    time_steps, sol = solve(solver, (0.0, 15.0), saveat = 0.2, Cₘ = 0.9)
+    writeToVTK(solution.location, sol, points, faces) # Save the solution
+    resetCells(Cells)
     h = [sol[end][5*i-4] for i in eachindex(Cells)]
     return LinearAlgebra.norm2(h)/sqrt(length(h)) ## Average thickness at t = 15.0
 end
