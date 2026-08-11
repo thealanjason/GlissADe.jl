2ff01fc05621ee5e07c4499775085e92f4cf8916
Tanish Jain <recklurker@gmail.com>
2026-08-11 15:20:51 +0530

    perf(rheology): annotate muIDefault with @inline @fastmath (#44)

--- src/module/solver/src/rheology_model/muIDefault.jl
+++ src/module/solver/src/rheology_model/muIDefault.jl
@@ -8,7 +8,7 @@ Last Updated On: 12th January, 2025 09:46 UTC+5:30
     muIDefault(Cells, h, vel, pb, alpha, zeta, rho, idx)
 A default basal stress computation based on muI rheology. `INTERNAL`
 """
-function muIDefault(Cell, h, vel, pb, alpha, zeta, rho)
+@inline @fastmath function muIDefault(Cell, h, vel, pb, alpha, zeta, rho)
     vel_mag = norm2(vel)
     vel_inv = 1.0 / (vel_mag + 1.0e-4)
     h_inv = 1.0 / (h + 1.0e-6)
